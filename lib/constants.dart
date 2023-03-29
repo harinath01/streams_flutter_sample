4d1aefb68a0ad4086d219468cdecee1cb4433a64
Hari <hari@testpress.in>
2023-03-29 17:04:52 +0530

    fix: Update valid non-DRM video asset ID

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,5 +2,5 @@ class Constants {
   static const ORG_CODE = 'e2fd4e';
   static const AUTHORIZATION_TOKEN = 'adc50c1616d48fe12be13c405b80297f6444d8c8effc5ff8bab1ae04f6082c69';
   static const DRM_ASSET_ID = '7cc145c6-0301-404f-a25d-6fb083a20f3a';
-  static const NON_DRM_ASSET_ID = 'eb80c975-dccc-4e7c-b7e6-e628e3cd49ad';
+  static const NON_DRM_ASSET_ID = 'cc94519d-bdea-41ce-8f41-8a5733cd7ddf';
 }
